d0fdfe8f68ac2f3f895db592c090181b7c55a57e
arshadeep <arshu111.as@gmail.com>
2021-05-12 19:03:57 +0800

    Validation-extra

--- StudentRecord/ViewModels/StudentEditViewModel.cs
+++ StudentRecord/ViewModels/StudentEditViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using StudentRecord.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,9 +11,13 @@ namespace StudentRecord.ViewModels
     {
         
         public int RollNo { get; set; }
+        [Required]
+        [MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter Age")]
+        [Range(3, 18)]
         public int Age { get; set; }
-
+        [Required(ErrorMessage = "Please enter Gender")]
         public Gender Gender { get; set; }
     }
 }
